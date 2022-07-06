aad2b0f58ac12a70367130176b9f15a5cc09d2c3
thang la <you@example.com>
2022-07-06 21:42:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+export const headers = {
+  'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+  'Accept': 'application/json'
+}
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const fetchColor = id => {
+    console.log('fetch color')
+    return fetch(`http://localhost:8000/api/colors/${id}`, {headers})
+  }
+  const fetchTodos = color => {
+    console.log('fetch todo')
+    return fetch(`http://localhost:8000/api/todos?pageSize=100&colors=${color}`, {headers})
+  }
+
+  const fetch1 = () => {
+    fetchColor(17)
+      .then(res => res.json())
+      .then(color => {
+        console.log('color: ', color)
+        fetchTodos(color.data.name)
+          .then(res => res.json())
+          .then(todos => {
+            console.log('todo: ', todos)
+            setTodos(todos.data)
+          })
+          .catch(error => {
+            console.log('fetch todo eror: ', error)
+          })
+      })
+      .catch(error => console.log('fetch color eror: ', error))
+    return
+  }
+
+  const fetch2 = () => {
+    let colorName
+    fetchColor(17)
+      .then(res => res.json())
+      .then(color => {
+        console.log('color: ', color)
+        colorName = color.data.name
+      })
+      .catch(error => console.log('fetch color eror: ', error))
+
+    fetchTodos(colorName)
+      .then(res => res.json())
+      .then(todos => {
+        console.log('todo: ', todos)
+        setTodos(todos.data)
+      })
+      .catch(error => console.log('fetch todo eror: ', error))
+  }
+
+  useEffect(() => {
+    // fetch1()
+    fetch2()
+  }, [])
+
+  // fetch2()
+
+  const handleClick = () => {
+    fetch1()
+    // fetch2()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Text</th>
+            <th>Color</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map(todo => (
+            <tr key={todo.id}>
+              <td>{todo.id}</td>
+              <td>{todo.text}</td>
+              <td>{todo.color?.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button onClick={handleClick}>Re Fetch</button>
     </div>
   );
 }
---
+++ src/App1.js
@@ -0,0 +1,57 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import { headers } from './App'
+
+const App1 = () => {
+    const [todos, setTodos] = useState([])
+
+    const fetchColor = id => {
+        return axios.get(`http://localhost:8000/api/colors/${id}`, {headers})
+    }
+
+    const fetchTodos = color => {
+        return axios.get(`http://localhost:8000/api/todos?pageSize=100&colors=${color}`, {headers})
+    }
+
+    const fetchData = async () => {
+        const response1 = await fetchColor(17)
+        const color = await response1.data
+        console.log('color: ', color)
+
+        const response2 = await fetchTodos(color.data.name)
+        const todos = await response2.data
+        setTodos(todos.data)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    console.log('todos: ', todos)
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Text</th>
+                        <th>Color</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {todos.map(todo => (
+                        <tr key={todo.id}>
+                        <td>{todo.id}</td>
+                        <td>{todo.text}</td>
+                        <td>{todo.color?.name}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <button>Re Fetch</button>
+        </div>
+    )
+}
+
+export default App1
